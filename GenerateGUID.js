16479c8ed5c75f343ee24f2de36dbda6212a09b6
Jonathan Opperman <jonathan.opperman@thesustainables.nl>
2022-12-25 15:22:39 +0200

    Output generated GUID

--- GenerateGUID.js
+++ GenerateGUID.js
@@ -1,5 +1,16 @@
-function CreateUUID() {
-  return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
-    (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+/* Generate a random GUID/UUID using ES6 crypto API
+ * and return it's value
+ */
+var crypto = require('crypto');
+var getMessage;
+var response;
+
+getMessage = function () {
+    return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+    (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16) 
   )
-}
+};
+
+response = getMessage();
+
+console.log(response);
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # es6-guid-generator
+
+## Pre-requisites
+- Nodejs
+
+## Usage
+``` node .\GenerateGUID.js ```
